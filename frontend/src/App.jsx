5dd4853d0ac444febb0c2fd29efc0223bfc17967
Evan M. Bonk <embonk@hotmail.com>
2025-08-29 09:52:51 -0600

    checkpoint 4

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,38 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
+  const [favorites, setFavorites] = useState([]);
+
+  useEffect(() => {
+    const getFavorites = async () => {
+      let data = await fetch('http://localhost:8080/movies').then((res) => res.json());
+      setFavorites(data);
+    }
+    getFavorites();
+    console.log(favorites)
+  }, [])
 
   return (
     <>
-
+      <div>
+        <ol>
+          <li>
+            {favorites.map((movie) => {
+              return (
+                <div>
+                  {movie.id}
+                  <ul>
+                    <li>{movie.title} </li>
+                    <li>{movie.main_character}</li>
+                    <li>{movie.year_released}</li>
+                  </ul>
+                </div>
+              )
+            })}
+          </li>
+        </ol>
+      </div>
     </>
   )
 }
